6ae4e75eb4d43345a366dd08ee8a8989c519c310
vsvang87 <vsvang@gmail.com>
2022-11-25 14:19:58 -0500

    11/25/22, added fade in on scroll

--- js/tesla.js
+++ js/tesla.js
@@ -1,10 +1,31 @@
+//Grabbing all my needed selectors
 const openBtn = document.getElementById("hamburger");
 const mobileMenu = document.getElementById("mobile-nav");
 const closeBtn = document.getElementById("close");
 
+//open the mobile menu when click on menu
 openBtn.addEventListener("click", () => {
   mobileMenu.classList.toggle("is-active");
 });
+//close mobile menu when click on x
 closeBtn.addEventListener("click", () => {
   mobileMenu.classList.remove("is-active");
 });
+
+//fade in on scroll
+window.addEventListener("scroll", reveal);
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let revealTop = reveals[i].getBoundingClientRect().top;
+    let revealPoint = 100;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
